54e084bc56c5ef68a335e4937062c90b645ba28d
“Bridgeshantanu” <shantanupadul@gmail.com>
2023-05-23 13:05:46 +0530

    Add: employee attendance code is added

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace EmployeeWageComputation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int attendance = random.Next(2);
+
+            if (attendance == 1)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
