e224436f21be4225fc3fcc5c43dfc1df117ebda0
“Bridgeshantanu” <shantanupadul@gmail.com>
2023-06-02 14:26:57 +0530

    Add: employee wage for multiple companies code is added

---
+++ Company.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputation
+{
+    internal class Company
+    {
+        public int wage;
+        public int workingDays;
+        public int workingHours;
+
+        public Company(int wage, int workingDays, int workingHours)
+        {
+            this.wage = wage;
+            this.workingDays = workingDays;
+            this.workingHours = workingHours;
+        }
+
+        public int CalculateEmployeeWage(int hoursWorked)
+        {
+            int dailyWage = (int)((wage * workingHours) / workingDays);
+            int totalWage = (int)((dailyWage * hoursWorked) / workingHours);
+            return totalWage;
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,10 +5,20 @@
          static void Main(string[] args)
          {
 
-            
             Employee employee = new Employee(); 
             employee.Checkattendance();
+
             
-         }
+            Company company1 = new Company(10, 20, 8);
+            Company company2 = new Company(15, 22, 7);
+
+            int employeeWage1 = company1.CalculateEmployeeWage(160);
+            Console.WriteLine($"Employee wage for company 1: {employeeWage1}");
+
+            int employeeWage2 = company2.CalculateEmployeeWage(154);
+            Console.WriteLine($"Employee wage for company 2: {employeeWage2}");
+
+
+        }
     }
 }
